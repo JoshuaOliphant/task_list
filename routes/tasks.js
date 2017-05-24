28e1cad0a65ac3da9b46766174a58da2261081a3
Joshua Oliphant <josholiphantlmp@gmail.com>
2017-05-23 18:50:06 -0700

    Added ability to get single task

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,12 +5,23 @@ var mongojs = require('mongojs');
 var db = mongojs('mongodb://josh:josh@ds041939.mlab.com:41939/mytasklistjosh', ['tasks']);
 
 // localhost:3000/api/tasks
+// get all tasks
 router.get('/tasks', function(req, res, next) {
     db.tasks.find(function(err, tasks) {
         if(err) {
             res.send(err);
         }
         res.json(tasks);
+    });
+});
+
+// get a single task
+router.get('/task/:id', function(req, res, next) {
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(task);
     });
 });
 
