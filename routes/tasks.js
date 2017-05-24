83e40dd1647127edbd08a306dcdba79b9129f4a5
Joshua Oliphant <josholiphantlmp@gmail.com>
2017-05-23 19:11:08 -0700

    Added delete and update

--- routes/tasks.js
+++ routes/tasks.js
@@ -45,4 +45,46 @@ router.post('/task', function (req, res, next) {
     }
 });
 
+// delete a single task
+router.delete('/task/:id', function (req, res, next) {
+    db.tasks.remove({
+        _id: mongojs.ObjectId(req.params.id)
+    }, function (err, task) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+// update a single task
+router.put('/task/:id', function (req, res, next) {
+    //get info from form first
+    var task = req.body;
+    var updTask = {};
+
+    if (task.isDone) {
+        updTask.isDone = task.isDone;
+    }
+    if (task.title) {
+        updTask.title = task.title;
+    }
+    if (!updTask) {
+        res.status = 400;
+        res.json = ({
+            "error": "bad data"
+        });
+    } else {
+        db.tasks.update({
+            _id: mongojs.ObjectId(req.params.id)
+        }, updTask, {}, function (err, task) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(task);
+        });
+    }
+});
+
+
 module.exports = router;
