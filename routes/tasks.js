322147d842947ad2f3d2cbc0258a7f7a5c300686
Joshua Oliphant <josholiphantlmp@gmail.com>
2017-05-23 19:00:21 -0700

    Added ability to save a task. Also reformatted with beautify.

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,9 +6,9 @@ var db = mongojs('mongodb://josh:josh@ds041939.mlab.com:41939/mytasklistjosh', [
 
 // localhost:3000/api/tasks
 // get all tasks
-router.get('/tasks', function(req, res, next) {
-    db.tasks.find(function(err, tasks) {
-        if(err) {
+router.get('/tasks', function (req, res, next) {
+    db.tasks.find(function (err, tasks) {
+        if (err) {
             res.send(err);
         }
         res.json(tasks);
@@ -16,13 +16,33 @@ router.get('/tasks', function(req, res, next) {
 });
 
 // get a single task
-router.get('/task/:id', function(req, res, next) {
-    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
-        if(err) {
+router.get('/task/:id', function (req, res, next) {
+    db.tasks.findOne({
+        _id: mongojs.ObjectId(req.params.id)
+    }, function (err, task) {
+        if (err) {
             res.send(err);
         }
         res.json(task);
     });
+});
+
+// save a task
+router.post('/task', function (req, res, next) {
+    var task = req.body;
+    if (!task.title || (task.isDone + '')) {
+        res.status(400);
+        res.json({
+            "error": "bad data"
+        });
+    } else {
+        db.tasks.save(task, function (err, task) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(task);
+        })
+    }
 });
 
 module.exports = router;
